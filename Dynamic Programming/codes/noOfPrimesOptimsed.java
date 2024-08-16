c2c06ed147b649e3572841d154fdb5e512e98454
nandiayan73 <nandiayan73@gmail.com>
2024-08-16 21:22:36 +0530

    changes

---
+++ Dynamic Programming/codes/noOfPrimesOptimsed.java
@@ -0,0 +1,37 @@
+class noOfPrimesOptimsed{
+    static int dp[];
+    public boolean isPrime(int i,int n)
+    {
+        boolean flag=false;
+        if(dp[i]==-1)
+            flag=true;
+        if(dp[i]==0)
+            return false;
+        int k=2,sum=i*k;
+        while(sum<n)
+        {
+            dp[sum]=0;
+            k++;
+            sum=i*k;
+        }
+        return flag;
+    }
+    public int totalPrimes(int n)
+    {
+        int primes=0;
+        for(int i=2;i<n;i++)
+        {
+            if(isPrime(i,n))
+            {
+                primes++;
+            }
+        }
+        return primes;
+    }
+    public int countPrimes(int n) {
+        dp=new int[n];
+        Arrays.fill(dp,-1);
+        int res=totalPrimes(n);
+        return res;
+    }
+}
